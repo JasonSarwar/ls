ce45f03c97eb8efefa3407b3141d66bf0dd89fb4
JasonJo1993 <jsarwar@stevens.edu>
2016-09-30 00:56:22 -0400

    Flag variables 2

--- ls.c
+++ ls.c
@@ -11,6 +11,10 @@
 
 int main(int argc, char **argv) {
 
+  int noOfFlags = 0;
+  int noOfFiles = 0;
+
+  //These integers will be 0 if the flag is off or 1 if the flag is on
   int A = 0;
   int a = 0;
   int C = 0;
@@ -34,7 +38,115 @@ int main(int argc, char **argv) {
   int x = 0;
   int one = 0;
 
-  printf("a = %i, A = %i\n", a, A);
+  int lpr = 0; //For loops
+
+  if(argc == 1) {
+    //Execute ls without any flags or files
+
+
+  }
+  else {
+
+    if(argv[2][0] == '-') {
+      lpr = 1;
+      while(argv[2][lpr] != '\0') {
+	if(argv[2][lpr] == 'A') {
+	  A = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'a') {
+	  a = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'c') {
+	  c = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'C') {
+	  C = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'd') {
+	  d = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'F') {
+	  F = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'f') {
+	  f = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'h') {
+	  h = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'i') {
+	  i = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'k') {
+	  k = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'l') {
+	  l = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'n') {
+	  n = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'q') {
+	  q = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'R') {
+	  R = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'r') {
+	  r = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'S') {
+	  S = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 's') {
+	  s = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 't') {
+	  t = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'u') {
+	  u = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'w') {
+	  w = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == 'x') {
+	  x = 1;
+	  noOfFlags++;
+	}
+	else if(argv[2][lpr] == '1') {
+	  one = 1;
+	  noOfFlags++;
+	}
+
+
+
+      }
+
+    }
+
+  }
+
 
   return 0;
 }
