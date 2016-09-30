5064eda2a6b728adbb1639fb04414f355283e37b
JasonJo1993 <jsarwar@stevens.edu>
2016-09-29 23:15:18 -0400

    Made variables for flags

--- ls.c
+++ ls.c
@@ -4,10 +4,37 @@
  * ls(1)
  */
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(int argc, char **argv) {
 
-  
+  int A = 0;
+  int a = 0;
+  int C = 0;
+  int c = 0;
+  int d = 0;
+  int F = 0;
+  int f = 0;
+  int h = 0;
+  int i = 0;
+  int k = 0;
+  int l = 0;
+  int n = 0;
+  int q = 0;
+  int R = 0;
+  int r = 0;
+  int S = 0;
+  int s = 0;
+  int t = 0;
+  int u = 0;
+  int w = 0;
+  int x = 0;
+  int one = 0;
 
+  printf("a = %i, A = %i\n", a, A);
 
   return 0;
 }
